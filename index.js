6c566e658336ce863a257b80e3fbda8fe97eadd5
Ruben <rubo.lopez.1999@alumnos.cei.es>
2024-06-05 18:11:17 +0200

    todo añadido

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
+const {json} = require("body-parser");
+const postgres = require("postgres");
 const servidor = express();
 
+servidor.use(json());
+
+if(process.env.LOCAL){
+    servidor.use(express.static("./pruebas"))
+}
 
 
 
-servidor.listen(4000);
+
+
+
+servidor.listen(process.env.PORT);
