1ee7e0bc2b01d558441ca25a23bfc1ce3e59fed2
Ruben <rubo.lopez.1999@alumnos.cei.es>
2024-06-06 16:38:14 +0200

    avances

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
+
 const {tareas} = require("./db");
 
 const express = require("express");
-
+const {json} = require("body-parser");
 const servidor = express();
 
-
+servidor.use(json())
 
 if(process.env.LOCAL){
     servidor.use(express.static("./pruebas"))
@@ -21,13 +22,23 @@ servidor.get("/tareas", async (peticion,respuesta) => {
 
         respuesta.json(error);
     }
-    
+})
+
+servidor.post("/tareas/nueva", (peticion,respuesta) => {
+    console.log(peticion.body);
+    respuesta.send("hola");
 
 })
 
 servidor.use((peticion,respuesta) => {
     respuesta.status(404);
     respuesta.json({error : "recurso no encontrado"});
+})
+
+servidor.use((error,peticion,respuesta,siguiente) => {
+    console.log(error);
+    respuesta.status(400);
+    respuesta.json({error:"error en la petición"});
 })
 
 
