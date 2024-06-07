b1822650a3ca1ef899cdfaae6471ee022a16f390
Ruben <rubo.lopez.1999@alumnos.cei.es>
2024-06-07 11:43:57 +0200

    API a puntito

--- index.js
+++ index.js
@@ -53,6 +53,26 @@ servidor.delete("/tareas/borrar/:id([0-9]+)", async (peticion,respuesta) => {
     }
 })
 
+servidor.put("/tareas/actualizar/:id([0-9]+)/:operacion(1|2)", async (peticion,respuesta,siguiente) => {
+    let operacion = Number(peticion.params.operacion);
+
+    let funciones = [editarTexto, toggleEstado];
+
+    if(operacion == 1 && (!peticion.body.tarea || peticion.body.tarea.trim() == "")){
+        return siguiente(true);
+    }
+    
+    try{
+        let count = await funciones[operacion - 1](peticion.params.id, operacion == 1 ? peticion.body.tarea : null );
+
+        respuesta.json({resultado : count ? "ok" : "ko"});
+    }catch(error){
+        respuesta.status(500);
+
+        respuesta.json(error); 
+    }
+})
+
 servidor.use((peticion,respuesta) => {
     respuesta.status(404);
     respuesta.json({error : "recurso no encontrado"});
@@ -62,7 +82,6 @@ servidor.use((error,peticion,respuesta,siguiente) => {
     respuesta.status(400);
     respuesta.json({error:"error en la petición"});
 })
-
 
 
 servidor.listen(process.env.PORT);
